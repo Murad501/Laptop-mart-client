e23fc5d61fe5726f011bf4a1bc4f073413af76b4
Murad501 <mm845635@gmail.com>
2022-11-24 02:31:06 +0600

    footer added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,61 @@
-import React from 'react';
+import React from "react";
+import { FaFacebook, FaTwitter, FaYoutube } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
+  return (
+    <div>
+      <footer className="footer p-10 border-t">
         <div>
-            footer
+          <span className="footer-title">Services</span>
+          <Link to="/" className="link link-hover">
+            Branding
+          </Link>
+          <Link to="/" className="link link-hover">
+            Design
+          </Link>
+          <Link to="/" className="link link-hover">
+            Marketing
+          </Link>
+          <Link to="/" className="link link-hover">
+            Advertisement
+          </Link>
         </div>
-    );
+        <div>
+          <span className="footer-title">Company</span>
+          <Link to="/" className="link link-hover">
+            About us
+          </Link>
+          <Link to="/" className="link link-hover">
+            Contact
+          </Link>
+          <Link to="/" className="link link-hover">
+            Jobs
+          </Link>
+          <Link to="/" className="link link-hover">
+            Press kit
+          </Link>
+        </div>
+        <div>
+          <span className="footer-title">Social</span>
+          <div className="grid grid-flow-col gap-4">
+            <Link>
+              <FaTwitter className="text-2xl text-primary"></FaTwitter>
+            </Link>
+            <Link>
+              <FaYoutube className="text-2xl text-primary"></FaYoutube>
+            </Link>
+            <Link>
+              <FaFacebook className="text-2xl text-primary"></FaFacebook>
+            </Link>
+          </div>
+        </div>
+      </footer>
+      <div className="py-5 text-center">
+        <p>Copyright © 2022 - All right reserved by Laptop Mart</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
