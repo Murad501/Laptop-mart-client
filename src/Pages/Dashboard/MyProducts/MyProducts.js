1b391d3ffdff0eec90b2d35ccb54063885a7d7a0
Murad501 <mm845635@gmail.com>
2022-11-26 01:05:04 +0600

    product deleted function completed

--- src/Pages/Dashboard/MyProducts/MyProducts.js
+++ src/Pages/Dashboard/MyProducts/MyProducts.js
@@ -3,18 +3,17 @@ import { useQuery } from "@tanstack/react-query";
 import Product from './Product';
 
 const MyProducts = () => {
-    const {data:products = []} = useQuery({
+    const {data:products = [], refetch} = useQuery({
         queryKey: ['/products'],
         queryFn: ()=>fetch('http://localhost:5000/products')
         .then(res => res.json())
     })
-    console.log(products.length)
     return (
         <div>
             <h1 className='text-center font-bold text-3xl my-5'>My Products</h1>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
                 {
-                    products.map(product=><Product key={product._id} product={product}></Product>)
+                    products.map(product=><Product key={product._id} refetch={refetch} product={product}></Product>)
                 }
             </div>
         </div>
--- src/Pages/Dashboard/MyProducts/Product.js
+++ src/Pages/Dashboard/MyProducts/Product.js
@@ -1,30 +1,85 @@
 import React from "react";
+import toast from "react-hot-toast";
 
-const Product = ({product}) => {
-    console.log(product)
-    const {name, image, sellingPrice, description, available, advertised} = product
+const Product = ({ product, refetch }) => {
+  const { name, image, sellingPrice, description, available, advertised, _id } =
+    product;
+
+  const handleAdvertised = (id) => {
+    fetch(`http://localhost:5000/product/${id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.matchedCount) {
+          refetch();
+          toast.success("Product advertised successfully");
+        }
+      });
+  };
+  
+  const handleDelete = (id) => {
+    fetch(`http://localhost:5000/product/${id}`, {
+        method: 'DELETE'
+    })
+    .then(res => res.json())
+    .then(data => {
+        if(data.deletedCount){
+            refetch()
+            toast.success('Product deleted successfully')
+        }
+    })
+  }
+
   return (
     <div className="card p-2 rounded-sm border hover:shadow-sm">
-        <h2 className="card-title my-3">{name.length > 50 ? name.slice(0, 50)+('...') : name}</h2>
+      <h2 className="card-title my-3">
+        {name.length > 50 ? name.slice(0, 50) + "..." : name}
+      </h2>
       <figure>
         <img className="w-full" src={image} alt="Product_Image" />
       </figure>
       <div className="card-body p-0">
-        <p><span className="text-xl">Description:</span> {description.length > 250 ? description.slice(0, 250)+('...'):description}</p>
+        <p>
+          <span className="text-xl">Description:</span>{" "}
+          {description.length > 250
+            ? description.slice(0, 250) + "..."
+            : description}
+        </p>
         <div className="my-5 flex justify-between items-center">
-        <p className="text-lg">Status: <span className="font-bold text-primary">{available ? 'Available' : 'Sold'}</span></p>
-        <p className="text-lg text-end">Price: <span className="font-bold text-primary">${sellingPrice}</span></p>
+          <p className="text-lg">
+            Status:{" "}
+            <span className="font-bold text-primary">
+              {available ? "Available" : "Sold"}
+            </span>
+          </p>
+          <p className="text-lg text-end">
+            Price:{" "}
+            <span className="font-bold text-primary">${sellingPrice}</span>
+          </p>
         </div>
         <div className="card-actions justify-between">
-          <button className="btn bg-red-600 text-white border-none">Delete</button>
-          {
-            advertised ? 
-            <button className={`btn border border-primary text-white ${!available && 'btn-disabled'}`}>Advertised</button>
-            :
-            <button className={`btn btn-primary text-white ${!available && 'btn-disabled'}`}>Advertise</button>
-            
-            
-          }
+          <button onClick={()=>handleDelete(_id)} className="btn bg-red-600 text-white border-none">
+            Delete
+          </button>
+          {advertised ? (
+            <button
+              className={`btn btn-success border border-primary text-white ${
+                !available && "btn-disabled"
+              }`}
+            >
+              Advertised
+            </button>
+          ) : (
+            <button
+              onClick={() => handleAdvertised(_id)}
+              className={`btn btn-primary text-white ${
+                !available && "btn-disabled"
+              }`}
+            >
+              Advertise
+            </button>
+          )}
         </div>
       </div>
     </div>
